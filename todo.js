6540b5444e6ea4b4b2c0e4433d34825c9ad011dd
Timmy Wertz <timmylwertz@gmail.com>
2018-06-07 16:39:42 -0400

    started handleCmd. accepting cli arguments

--- todo.js
+++ todo.js
@@ -1,6 +1,23 @@
-const R = require('Ramda')
+const { head } = require('Ramda')
 
+const [execPath, jsPath, cmd, ...rest] = process.argv
 
-const things = ["foo", 8, null, false, "bar"]
+// process.argv.forEach((val, index) => {
+//     console.log(`${index}: ${val}`)
+// })
 
-console.log(R.head(things))
+console.log(handleCmd(cmd))
+
+function handleCmd(cmd) {
+	// command: 'add', 'init', 'rm'
+	switch (cmd) {
+		case 'init':
+			return 'you asked for the init cmd'
+		case 'add':
+			return 'you asked for the add cmd'
+		case 'rm':
+			return 'you asked for the rm cmd'
+		default:
+			return `${cmd} is not supported`
+	}
+}
